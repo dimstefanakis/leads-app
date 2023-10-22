0704733051176f4ab3ed1fd029f492458e00f79d
Dimitris <jimstef@outlook.com>
2023-10-22 04:36:32 +0300

    debugging

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -13,47 +13,52 @@ const openai = new OpenAI({
 export const runtime = 'edge'
 
 export async function POST(req: Request) {
-  const { messages } = await req.json()
-  const supabase = createRouteHandlerClient<Database>({ cookies })
-  const { data: { user } } = await supabase.auth.getUser()
-  if (!user) {
-    return NextResponse.json({ error: 'You need to be signed in' })
-  }
+  try {
+    const { messages } = await req.json()
+    const supabase = createRouteHandlerClient<Database>({ cookies })
+    const { data: { user } } = await supabase.auth.getUser()
+    if (!user) {
+      return NextResponse.json({ error: 'You need to be signed in' })
+    }
 
-  // @ts-ignore
-  const requestCount = await supabase.rpc('get_monthly_chat_request_count', {
-    p_user_id: user.id,
-  })
+    // @ts-ignore
+    const requestCount = await supabase.rpc('get_monthly_chat_request_count', {
+      p_user_id: user.id,
+    })
 
-  if (requestCount.error) {
+    if (requestCount.error) {
+      return NextResponse.json({ error: 'An error has occured' }), { status: 500 }
+    }
+
+    const subscriptionResponse = await supabase
+      .from('subscriptions')
+      .select('*, prices(*, products(*))')
+      .eq('user_id', user?.id)
+      // .in('status', ['trialing', 'active'])
+      .single();
+
+    //@ts-ignore
+    const hasProPlan = subscriptionResponse.data?.prices?.products?.metadata?.type === 'premium'
+
+    if (hasProPlan && requestCount.data > 400 || !hasProPlan && requestCount.data > 40) {
+      return NextResponse.json({ error: 'You have reached your monthly chat request limit' }), { status: 500 }
+    }
+
+    // log chat request
+    await supabase.from('chat_requests').insert({
+      user_id: user.id,
+    }).select('*')
+
+    const response = await openai.chat.completions.create({
+      model: hasProPlan ? 'gpt-4' : 'gpt-3.5-turbo',
+      // model: 'gpt-4',
+      stream: true,
+      messages
+    })
+    const stream = OpenAIStream(response)
+    return new StreamingTextResponse(stream)
+  } catch (err) {
+    console.log(err)
     return NextResponse.json({ error: 'An error has occured' }), { status: 500 }
   }
-
-  const subscriptionResponse = await supabase
-    .from('subscriptions')
-    .select('*, prices(*, products(*))')
-    .eq('user_id', user?.id)
-    // .in('status', ['trialing', 'active'])
-    .single();
-
-  //@ts-ignore
-  const hasProPlan = subscriptionResponse.data?.prices?.products?.metadata?.type === 'premium'
-
-  if (hasProPlan && requestCount.data > 400 || !hasProPlan && requestCount.data > 40) {
-    return NextResponse.json({ error: 'You have reached your monthly chat request limit' }), { status: 500 }
-  }
-
-  // log chat request
-  await supabase.from('chat_requests').insert({
-    user_id: user.id,
-  }).select('*')
-
-  const response = await openai.chat.completions.create({
-    model: hasProPlan ? 'gpt-4' : 'gpt-3.5-turbo',
-    // model: 'gpt-4',
-    stream: true,
-    messages
-  })
-  const stream = OpenAIStream(response)
-  return new StreamingTextResponse(stream)
 }
--- src/hooks/useHasProPlan.tsx
+++ src/hooks/useHasProPlan.tsx
@@ -2,8 +2,8 @@ import { useUser } from "@/lib/useUser";
 
 function useHasProPlan() {
   const { user, subscription } = useUser();
-
   const hasProPlan = subscription?.prices?.products?.metadata?.plan == 'premium';
+  console.log('user', hasProPlan, subscription);
 
   return hasProPlan;
 }
